e6dcd2d589152216415c08b59f22d00accbce51c
Moonspot011 <145489380+Moonspot011@users.noreply.github.com>
2024-08-14 08:29:24 -0500

    Update 3.c

--- PP_1/3.c
+++ PP_1/3.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    char nombre[50]; // Se declara un arreglo de caracteres para almacenar el nombre
-
-    // Se pide al usuario que introduzca su nombre
+    char nombre[50];
+   
     printf("Por favor, introduce tu nombre: ");
-    fgets(nombre, sizeof(nombre), stdin); // Se utiliza fgets para leer el nombre
+    fgets(nombre, sizeof(nombre), stdin); 
 
-    // Se muestra el saludo
     printf("¡Hola %s", nombre);
 
     return 0;
